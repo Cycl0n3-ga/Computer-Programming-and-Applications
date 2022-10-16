18abb64b2c0909dea41c1c487a85b740ab200ca0
cyclonegao <jdio1881@gmail.com>
2022-10-16 22:49:57 +0800

    add README and HW5

---
+++ HW5/HW5.c
@@ -0,0 +1,27 @@
+/*****************
+ * Author: CHUNG-NING, KAO
+ * FileName: HW5.c, a.out
+ * Version:1.0	
+ * Contact: B11002108@mail.ntust.edu.tw
+ * Date: Oct.16.2022
+ * ***************/
+
+#include <stdio.h>
+
+int main(){
+
+    int iteration_times = 0;
+    int i = 0;
+    float div = 1;
+    double exp_o = 1.0;
+
+    printf("How Many Times u WANT!: ");
+    scanf("%d",&iteration_times);
+
+    for(i=1;i<iteration_times+1;i++){
+	div = i*div;
+	exp_o = exp_o + 1/div;
+	printf("%d. e = %5.10f      \n",i ,exp_o);
+    }
+}
+//float p
---
+++ HW5/README.md
@@ -0,0 +1,5 @@
+# This is HW for Computer Programming and Applications 
+
+Semester: 111,1  
+Teacher: 林敬舜  
+Course Code: ET3405302  
